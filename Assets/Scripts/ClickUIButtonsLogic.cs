f4639f2cd5676686f662781546c1250e60f0fd9c
Danilingous <nikdanil98@bk.ru>
2023-06-10 13:05:28 +0300

    Game is ready.

--- Assets/Scripts/ClickUIButtonsLogic.cs
+++ Assets/Scripts/ClickUIButtonsLogic.cs
@@ -6,6 +6,7 @@ public class ClickUIButtonsLogic : MonoBehaviour
     [SerializeField] private BirdAddForce _birdAddForce;
     [SerializeField] private StartGameLogic _startGameLogic;
     [SerializeField] private SoundMode _soundMode;
+    [SerializeField] private CanvasesManager _canvasesManager;
 
     public void ButtonAddForceClick() => _birdAddForce.AddForcePerTouch();
 
@@ -14,6 +15,28 @@ public class ClickUIButtonsLogic : MonoBehaviour
     public void ButtonMuteOnClick() => _soundMode.MuteOn();
     public void ButtonMuteOffClick() => _soundMode.MuteOff();
     public void ButtonPlayAgainClick()=>SceneManager.LoadScene(0);
-    
+    public void ButtonGameModesClick() => _canvasesManager.OnCanvasChooseGameMode();
+
+    public void ButtonEasyModeClick()
+    {
+        GameMode.Instance.SetEasyMode();
+        PlayerPrefs.SetInt("GameMode", 0);
+        SceneManager.LoadScene(0);
+    }
+
+    public void ButtonHardModeClick()
+    {
+        GameMode.Instance.SetHardMode();
+        PlayerPrefs.SetInt("GameMode", 1);
+        SceneManager.LoadScene(0);
+    }
+    public void ButtonVeryHardModeClick()
+    {
+        GameMode.Instance.SetVeryHardMode();
+        PlayerPrefs.SetInt("GameMode", 2);
+        SceneManager.LoadScene(0);
+    }
+
+
 
 }
